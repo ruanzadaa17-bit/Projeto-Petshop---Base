f96020ac0c3fa05560d4ca22011f45e1d7c70efc
ruanzadaa17-bit <ruanzadaa17@gmail.com>
2026-05-19 11:24:07 -0300

    Aprimoramos o petshop com várias mudanças propostas pelo professor e produzimos o readme.md

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 // scripts petshop
 
-document.getElementById("formContato").addEventListener("submit", function (e){
-e.preventDefault();
+document.getElementById("formContato").addEventListener("submit", function (e) {
+  e.preventDefault();
 
-let nome = document.getElementById("nome").value.trim();
-let email = document.getElementById("email").value.trim();
-let mensagem = document.getElementById("mensagem").value.trim();
+  let nome = document.getElementById("nome").value.trim();
+  let email = document.getElementById("email").value.trim();
+  let mensagem = document.getElementById("mensagem").value.trim();
 
-if(nome =="" || email == "" || mensagem =="") {
+  if (nome == "" || email == "" || mensagem == "") {
     alert("Preencha todos os campos!");
     return;
-}else{
-    alert("Mensagem enviada com sucesso!")
-}
+  } else {
+    alert("Mensagem enviada com sucesso!");
+  }
 
-this.reset();
+  this.reset();
 });
 
 // ************************************** //
@@ -23,7 +23,33 @@ let botaoMenu = document.getElementById("menu-btn");
 let menu = document.getElementById("menu");
 
 botaoMenu.addEventListener("click", () => {
-menu.classList.toggle("ativo")
-})
+  menu.classList.toggle("ativo");
+});
+// =========================
+// DARK MODE
+// =========================
 
-// ************************************** //
+const darkBtn = document.getElementById("darkModeBtn");
+const logo = document.getElementById("logo");
+
+darkBtn.addEventListener("click", () => {
+
+  document.body.classList.toggle("dark-mode");
+
+  // MODO ESCURO
+  if (document.body.classList.contains("dark-mode")) {
+
+    darkBtn.innerHTML = "☀️";
+
+    // troca logo
+    logo.src = "img/logo-dark.png";
+
+  } else {
+
+    darkBtn.innerHTML = "🌙";
+
+    // volta logo clara
+    logo.src = "img/logo-light.png";
+  }
+
+});
